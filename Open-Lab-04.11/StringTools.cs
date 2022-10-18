1f6e11faceca0269cc96146162bd5472a7ec0a49
einnarth <113421210+einnarth@users.noreply.github.com>
2022-10-18 09:55:29 +0200

    Update StringTools.cs

--- Open-Lab-04.11/StringTools.cs
+++ Open-Lab-04.11/StringTools.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace Open_Lab_04._11
 {
@@ -6,7 +7,7 @@ namespace Open_Lab_04._11
     {
         public string AlphabetSoup(string str)
         {
-            throw new NotImplementedException();
+            return str = string.Concat(str.OrderBy(ch => ch));
         }
     }
 }
